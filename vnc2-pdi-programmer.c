0820c07f4fc846bf2b77aa957d7b0c3598f61447
Jacob Quant <jacobq@gmail.com>
2019-03-03 05:39:38 -0600

    Generated project with VinIDE v2.0.2-SP2

---
+++ vnc2-pdi-programmer.c
@@ -0,0 +1,279 @@
+/*
+** Filename: vnc2-pdi-programmer.c
+**
+** Automatically created by Application Wizard 2.0.2
+** 
+** Part of solution vnc2-pdi-programmer in project vnc2-pdi-programmer
+**
+** Comments: 
+**
+** Important: Sections between markers "FTDI:S*" and "FTDI:E*" will be overwritten by
+** the Application Wizard
+*/
+
+#include "vnc2-pdi-programmer.h"
+
+/* FTDI:STP Thread Prototypes */
+vos_tcb_t *tcbFIRMWARE;
+vos_tcb_t *tcbTHREADFILEREADER;
+
+void firmware();
+void threadFileReader();
+/* FTDI:ETP */
+
+/* FTDI:SDH Driver Handles */
+VOS_HANDLE hUSBHOST_1; // USB Host Port 1
+VOS_HANDLE hUSBHOST_2; // USB Host Port 2
+VOS_HANDLE hGPIO_PORT_B; // GPIO Port B Driver
+VOS_HANDLE hTIMER_1; // Timer 1 Driver
+VOS_HANDLE hFAT_FILE_SYSTEM_1; // FAT File System for FAT32 and FAT16
+VOS_HANDLE hFAT_FILE_SYSTEM_2; // FAT File System for FAT32 and FAT16
+VOS_HANDLE hBOMS_1; // Bulk Only Mass Storage for USB disks
+VOS_HANDLE hBOMS_2; // Bulk Only Mass Storage for USB disks
+/* FTDI:EDH */
+
+/* Declaration for IOMUx setup function */
+void iomux_setup(void);
+
+/* Main code - entry point to firmware */
+void main(void)
+{
+	/* FTDI:SDD Driver Declarations */
+	// GPIO Port B configuration context
+	gpio_context_t gpioContextB;
+	// Timer 1 configuration context
+	tmr_context_t tmrContext1;
+	// USB Host configuration context
+	usbhost_context_t usbhostContext;
+	/* FTDI:EDD */
+
+	/* FTDI:SKI Kernel Initialisation */
+	vos_init(50, VOS_TICK_INTERVAL, VOS_NUMBER_DEVICES);
+	vos_set_clock_frequency(VOS_48MHZ_CLOCK_FREQUENCY);
+	vos_set_idle_thread_tcb_size(512);
+	/* FTDI:EKI */
+
+	iomux_setup();
+
+	/* FTDI:SDI Driver Initialisation */
+	// Initialise GPIO B
+	gpioContextB.port_identifier = GPIO_PORT_B;
+	gpio_init(VOS_DEV_GPIO_PORT_B,&gpioContextB);
+	
+	// Initialise Timer 1
+	tmrContext1.timer_identifier = 1;
+	tmr_init(VOS_DEV_TIMER_1,&tmrContext1);
+	
+	// Initialise FAT File System Driver
+	fatdrv_init(VOS_DEV_FAT_FILE_SYSTEM_1);
+	
+	// Initialise FAT File System Driver
+	fatdrv_init(VOS_DEV_FAT_FILE_SYSTEM_2);
+	
+	// Initialise BOMS Device Driver
+	boms_init(VOS_DEV_BOMS_1);
+	
+	// Initialise BOMS Device Driver
+	boms_init(VOS_DEV_BOMS_2);
+	
+	
+	
+	// Initialise USB Host
+	usbhostContext.if_count = 8;
+	usbhostContext.ep_count = 16;
+	usbhostContext.xfer_count = 2;
+	usbhostContext.iso_xfer_count = 2;
+	usbhost_init(VOS_DEV_USBHOST_1, VOS_DEV_USBHOST_2, &usbhostContext);
+	/* FTDI:EDI */
+
+	/* FTDI:SCT Thread Creation */
+	tcbFIRMWARE = vos_create_thread_ex(20, 4096, firmware, "Application", 0);
+	tcbTHREADFILEREADER = vos_create_thread_ex(24, 1024, threadFileReader, "FileReader", 0);
+	/* FTDI:ECT */
+
+	vos_start_scheduler();
+
+main_loop:
+	goto main_loop;
+}
+
+/* FTDI:SSP Support Functions */
+
+unsigned char usbhost_connect_state(VOS_HANDLE hUSB)
+{
+	unsigned char connectstate = PORT_STATE_DISCONNECTED;
+	usbhost_ioctl_cb_t hc_iocb;
+
+	if (hUSB)
+	{
+		hc_iocb.ioctl_code = VOS_IOCTL_USBHOST_GET_CONNECT_STATE;
+		hc_iocb.get        = &connectstate;
+		vos_dev_ioctl(hUSB, &hc_iocb);
+
+    // repeat if connected to see if we move to enumerated
+		if (connectstate == PORT_STATE_CONNECTED)
+		{
+			vos_dev_ioctl(hUSB, &hc_iocb);
+		}
+	}
+	return connectstate;
+}
+
+
+VOS_HANDLE fat_attach(VOS_HANDLE hMSI, unsigned char devFAT)
+{
+	fat_ioctl_cb_t           fat_ioctl;
+	fatdrv_ioctl_cb_attach_t fat_att;
+	VOS_HANDLE hFAT;
+
+	// currently the MSI (BOMS or other) must be open
+        // open the FAT driver
+	hFAT = vos_dev_open(devFAT);
+
+        // attach the FAT driver to the MSI driver
+	fat_ioctl.ioctl_code = FAT_IOCTL_FS_ATTACH;
+	fat_ioctl.set = &fat_att;
+	fat_att.msi_handle = hMSI;
+	fat_att.partition = 0;
+
+	if (vos_dev_ioctl(hFAT, &fat_ioctl) != FAT_OK)
+	{
+                // unable to open the FAT driver
+		vos_dev_close(hFAT);
+		hFAT = NULL;
+	}
+
+	return hFAT;
+}
+
+void fat_detach(VOS_HANDLE hFAT)
+{
+	fat_ioctl_cb_t           fat_ioctl;
+
+	if (hFAT)
+	{
+		fat_ioctl.ioctl_code = FAT_IOCTL_FS_DETACH;
+		fat_ioctl.set = NULL;
+		fat_ioctl.get = NULL;
+
+		vos_dev_ioctl(hFAT, &fat_ioctl);
+		vos_dev_close(hFAT);
+	}
+}
+
+
+VOS_HANDLE boms_attach(VOS_HANDLE hUSB, unsigned char devBOMS)
+{
+	usbhost_device_handle_ex ifDisk = 0;
+	usbhost_ioctl_cb_t hc_iocb;
+	usbhost_ioctl_cb_class_t hc_iocb_class;
+	msi_ioctl_cb_t boms_iocb;
+	boms_ioctl_cb_attach_t boms_att;
+	VOS_HANDLE hBOMS;
+
+	// find BOMS class device
+	hc_iocb_class.dev_class = USB_CLASS_MASS_STORAGE;
+	hc_iocb_class.dev_subclass = USB_SUBCLASS_MASS_STORAGE_SCSI;
+	hc_iocb_class.dev_protocol = USB_PROTOCOL_MASS_STORAGE_BOMS;
+
+	// user ioctl to find first hub device
+	hc_iocb.ioctl_code = VOS_IOCTL_USBHOST_DEVICE_FIND_HANDLE_BY_CLASS;
+	hc_iocb.handle.dif = NULL;
+	hc_iocb.set = &hc_iocb_class;
+	hc_iocb.get = &ifDisk;
+
+	if (vos_dev_ioctl(hUSB, &hc_iocb) != USBHOST_OK)
+	{
+		return NULL;
+	}
+
+	// now we have a device, intialise a BOMS driver with it
+	hBOMS = vos_dev_open(devBOMS);
+
+	// perform attach
+	boms_att.hc_handle = hUSB;
+	boms_att.ifDev = ifDisk;
+
+	boms_iocb.ioctl_code = MSI_IOCTL_BOMS_ATTACH;
+	boms_iocb.set = &boms_att;
+	boms_iocb.get = NULL;
+
+	if (vos_dev_ioctl(hBOMS, &boms_iocb) != MSI_OK)
+	{
+		vos_dev_close(hBOMS);
+		hBOMS = NULL;
+	}
+
+	return hBOMS;
+}
+
+void boms_detach(VOS_HANDLE hBOMS)
+{
+	msi_ioctl_cb_t boms_iocb;
+
+	if (hBOMS)
+	{
+		boms_iocb.ioctl_code = MSI_IOCTL_BOMS_DETACH;
+		boms_iocb.set = NULL;
+		boms_iocb.get = NULL;
+
+		vos_dev_ioctl(hBOMS, &boms_iocb);
+		vos_dev_close(hBOMS);
+	}
+}
+
+/* FTDI:ESP */
+
+void open_drivers(void)
+{
+        /* Code for opening and closing drivers - move to required places in Application Threads */
+        /* FTDI:SDA Driver Open */
+        hUSBHOST_1 = vos_dev_open(VOS_DEV_USBHOST_1);
+        hUSBHOST_2 = vos_dev_open(VOS_DEV_USBHOST_2);
+        hGPIO_PORT_B = vos_dev_open(VOS_DEV_GPIO_PORT_B);
+        hTIMER_1 = vos_dev_open(VOS_DEV_TIMER_1);
+        /* FTDI:EDA */
+}
+
+void attach_drivers(void)
+{
+        /* FTDI:SUA Layered Driver Attach Function Calls */
+        hBOMS_1 = boms_attach(hUSBHOST_1, VOS_DEV_BOMS_1);
+        hBOMS_2 = boms_attach(hUSBHOST_2, VOS_DEV_BOMS_2);
+        hFAT_FILE_SYSTEM_1 = fat_attach(hBOMS_1, VOS_DEV_FAT_FILE_SYSTEM_1);
+        hFAT_FILE_SYSTEM_2 = fat_attach(hBOMS_2, VOS_DEV_FAT_FILE_SYSTEM_2);
+        // TODO attach stdio to file system and stdio interface
+        //fsAttach(hFAT_FILE_SYSTEM); // VOS_HANDLE for file system (typically FAT)
+        //stdioAttach(hUART); // VOS_HANDLE for stdio interface (typically UART)
+        // TODO attach stdio to file system and stdio interface
+        //fsAttach(hFAT_FILE_SYSTEM); // VOS_HANDLE for file system (typically FAT)
+        //stdioAttach(hUART); // VOS_HANDLE for stdio interface (typically UART)
+        /* FTDI:EUA */
+}
+
+void close_drivers(void)
+{
+        /* FTDI:SDB Driver Close */
+        vos_dev_close(hUSBHOST_1);
+        vos_dev_close(hUSBHOST_2);
+        vos_dev_close(hGPIO_PORT_B);
+        vos_dev_close(hTIMER_1);
+        /* FTDI:EDB */
+}
+
+/* Application Threads */
+
+void firmware()
+{
+	/* Thread code to be added here */
+
+
+}
+
+void threadFileReader()
+{
+	/* Thread code to be added here */
+
+
+}
+
---
+++ vnc2-pdi-programmer.h
@@ -0,0 +1,48 @@
+/*
+** Filename: vnc2-pdi-programmer.h
+**
+** Automatically created by Application Wizard 2.0.2
+** 
+** Part of solution vnc2-pdi-programmer in project vnc2-pdi-programmer
+**
+** Comments: 
+**
+** Important: Sections between markers "FTDI:S*" and "FTDI:E*" will be overwritten by
+** the Application Wizard
+*/
+
+#ifndef _vnc2-pdi-programmer_H_
+#define _vnc2-pdi-programmer_H_
+
+#include "vos.h"
+
+/* FTDI:SHF Header Files */
+#include "USB.h"
+#include "USBHost.h"
+#include "GPIO.h"
+#include "Timers.h"
+#include "FAT.h"
+#include "msi.h"
+#include "BOMS.h"
+#include "stdio.h"
+#include "errno.h"
+#include "unistd.h"
+/* FTDI:EHF */
+
+/* FTDI:SDC Driver Constants */
+#define VOS_DEV_USBHOST_1 0
+#define VOS_DEV_USBHOST_2 1
+#define VOS_DEV_GPIO_PORT_B 2
+#define VOS_DEV_TIMER_1 3
+#define VOS_DEV_FAT_FILE_SYSTEM_1 4
+#define VOS_DEV_FAT_FILE_SYSTEM_2 5
+#define VOS_DEV_BOMS_1 6
+#define VOS_DEV_BOMS_2 7
+
+#define VOS_NUMBER_DEVICES 8
+/* FTDI:EDC */
+
+/* FTDI:SXH Externs */
+/* FTDI:EXH */
+
+#endif /* _vnc2-pdi-programmer_H_ */
